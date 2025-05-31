ecc05f2e3c0d6298a2db71348609ef99cc0c3da7
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2025-05-31 18:56:07 +0330

    feat: solve this question before gitting

--- medium/maximumSubArray.js
+++ medium/maximumSubArray.js
@@ -7,7 +7,7 @@ var maxSubArray = function (nums) {
     let end = 0
     let currentMax = nums[0]
     let totalMax = nums[0]
-    let start = 0
+    let start = 0 
     for (let i = 1; i < nums.length; i++) {
         if (currentMax + nums[i] > nums[i]) {
             currentMax = currentMax + nums[i]
