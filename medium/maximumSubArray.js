91fc136ee3bde47a0a52cfd620d34d23350fe3e0
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2025-05-31 18:54:31 +0330

    https://leetcode.com/problems/add-two-numbers/

---
+++ medium/maximumSubArray.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+    let tempStart = 0;
+    let end = 0
+    let currentMax = nums[0]
+    let totalMax = nums[0]
+    let start = 0
+    for (let i = 1; i < nums.length; i++) {
+        if (currentMax + nums[i] > nums[i]) {
+            currentMax = currentMax + nums[i]
+        } else {
+            currentMax = nums[i];
+            tempStart = i
+        }
+        if (currentMax > totalMax) {
+            totalMax = currentMax;
+            start = tempStart;
+            end = i
+        }
+    }
+
+    return totalMax
+};
